08aed3ea8c77ef26578e1e02846f4a99ea717e90
Ali Shahidi <63677039+Alishahidi@users.noreply.github.com>
2021-01-23 07:47:07 +0000

    Update ip.php
    FIx IP bug

--- server/core/ip.php
+++ server/core/ip.php
@@ -7,8 +7,36 @@ class ip{
 
     public static function get_info_from_ip(){
         $curl = curl_init();
+        function getUserIP()
+        {
+            // Get real visitor IP
+            if (isset($_SERVER["HTTP_CF_CONNECTING_IP"]))
+            {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            }
+            $client  = @$_SERVER['HTTP_CLIENT_IP'];
+            $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+            $remote  = $_SERVER['REMOTE_ADDR'];
+
+            if(filter_var($client, FILTER_VALIDATE_IP))
+            {
+                $ip = $client;
+            }
+            elseif(filter_var($forward, FILTER_VALIDATE_IP))
+            {
+                $ip = $forward;
+            }
+            else
+            {
+                $ip = $remote;
+            }
+            return $ip;
+        }
+
+        $ip = getUserIP();
         curl_setopt_array($curl, array(
-        CURLOPT_URL => "http://ip-api.com/json/?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query",
+        CURLOPT_URL => "http://ip-api.com/json/$ip?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 30,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
